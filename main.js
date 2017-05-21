933a57df2ce4a458e88ea893ab147437f55eb530
Dan <DecaHub@users.noreply.github.com>
2017-05-21 16:21:32 -0400

    Bypass the cache
    As the local cache is indexed by URL, every new request keeps displaying the same quote. We need to make them unique. 
    A cross-browser compatible approach to bypassing the cache is appending a timestamp to the URL, being sure to include a "?" or "&" as appropriate. This causes every request to be unique, thereby bypassing the cache.
    
    https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
    
    We are not going to use: 
    
    $.ajaxSetup({ cache: false });
    
    since that will disable the caching globally and there may be other calls where we would like to cache. Therefore, this uniqueness pattern allows us to bypass the cache for this single call.

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
-
-function getQuote (){
-    var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-    $.getJSON(url, function (data){
-        
-     $(".quote").html(data[0].content);
-     $(".author").html("- " + data[0].title)
-     
-    });
-};
-$(".btn").on("click", function(){
-    getQuote();
-});
+	
+	function getQuote (){
+		
+		var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+		
+		$.getJSON(url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), function (data){
+			
+			$(".quote").html(data[0].content);
+			$(".author").html("- " + data[0].title)
+		
+		});
+	
+	}
+	
+	$(".btn").on("click", function(){
+		
+		getQuote();
+	
+	});
+	
 });
